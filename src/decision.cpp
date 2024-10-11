d67b3d4d1e937a71c21c76994bd43d4dd433e374
shinepeach123 <1821934740@qq.com>
2024-10-11 12:38:51 +0800

    修改小bug

--- src/decision.cpp
+++ src/decision.cpp
@@ -1,17 +1,15 @@
 
 #include "state_machine_.h"
-#include "try.h"
 int main() {
-    trytry_need(2);
     
-    //fsm.processEvent(Event::QR_CODE_READ);
-    // fsm.processEvent(Event::BATCH_FETCHED);
-    // fsm.processEvent(Event::BATCH_DELIVERED);
-    // fsm.processEvent(Event::BATCH_STORED);
-    // fsm.processEvent(Event::BATCH_FETCHED);
-    // fsm.processEvent(Event::BATCH_DELIVERED);
-    // fsm.processEvent(Event::BATCH_STORED);
-    // fsm.processEvent(Event::RETURNED_TO_START);
+    fsm.processEvent(Event::QR_CODE_READ);
+    fsm.processEvent(Event::BATCH_FETCHED);
+    fsm.processEvent(Event::BATCH_DELIVERED);
+    fsm.processEvent(Event::BATCH_STORED);
+    fsm.processEvent(Event::BATCH_FETCHED);
+    fsm.processEvent(Event::BATCH_DELIVERED);
+    fsm.processEvent(Event::BATCH_STORED);
+    fsm.processEvent(Event::RETURNED_TO_START);
 
     return 0;
 }
